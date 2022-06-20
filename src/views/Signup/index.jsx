7573c03225d49880915a4a7f7bc193105601fb9e
Haris Souici <souici.haris@gmail.com>
2022-06-20 16:39:08 +0200

    feat: Sign Up form

--- src/views/Signup/index.jsx
+++ src/views/Signup/index.jsx
@@ -1,9 +1,51 @@
+import React from 'react'
+
 const Signup = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold">Signup</h1>
+    <div className="flex flex-col mt-12">
+      <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center px-2">
+        <div className="bg-gray-700 px-6 py-8 rounded shadow-md text-white w-full">
+          <h1 className="mb-8 text-3xl text-center">Inscrit toi avec ton mail HETIC</h1>
+          <input 
+            type="text"
+            className="block border border-grey-light w-full p-3 rounded mb-4"
+            name="fullname"
+            placeholder="Prénom"
+          />
+          <input 
+            type="text"
+            className="block border border-grey-light w-full p-3 rounded mb-4"
+            name="name"
+            placeholder="Nom"
+          />
+          <input 
+            type="text"
+            className="block border border-grey-light w-full p-3 rounded mb-4"
+            name="email"
+            placeholder="Email"
+          />
+          <input 
+            type="password"
+            className="block border border-grey-light w-full p-3 rounded mb-4"
+            name="password"
+            placeholder="Mot de passe"
+          />
+          <input 
+            type="password"
+            className="block border border-grey-light w-full p-3 rounded mb-4"
+            name="confirm_password"
+            placeholder="Confirme ton mot de passe"
+          />
+          <button
+              type="submit"
+              className="w-full text-center py-3 rounded bg-white text-black hover:bg-green-dark focus:outline-none my-1"
+          >
+            Créer ton compte
+          </button>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
 export default Signup
