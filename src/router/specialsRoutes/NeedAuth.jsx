5f47cbd5fdb3fef954319c69ade382c2302613f7
Haris Souici <souici.haris@gmail.com>
2022-06-21 10:23:29 +0200

    fix: Delete trema

--- src/router/specialsRoutes/NeedAuth.jsx
+++ src/router/specialsRoutes/NeedAuth.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-import PropTypes from 'prop-types';
+import React from "react"
+import { Navigate } from "react-router-dom"
+import PropTypes from 'prop-types'
 
 const NeedAuth = ({ children }) => {
   const isAuth = true
   if (!isAuth) {
-    return <Navigate to="/" />;
+    return <Navigate to="/" />
   }
-  return children;
-};
+  return children
+}
 
 NeedAuth.propTypes = {
   children: PropTypes.element
