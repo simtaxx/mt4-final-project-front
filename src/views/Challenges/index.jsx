80c93714d70e97e50ae64c82cc610646560ff7c2
Haris Souici <souici.haris@gmail.com>
2022-07-12 17:43:48 +0200

    feat: challenges features

--- src/views/Challenges/index.jsx
+++ src/views/Challenges/index.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState, useEffect } from 'react'
-import { useNavigate, useLocation, Link } from 'react-router-dom'
+import { useNavigate, useLocation } from 'react-router-dom'
 import { fetchChallenges } from '../../api'
 import UserContext from '../../contexts/user-context'
 import LoadingContext from '../../contexts/loading-context'
