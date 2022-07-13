6d4e18b179b4544db70c6c8fb81ed610559e9004
Kilian <marechal.kilian@gmail.com>
2022-07-13 14:17:45 +0200

    Home flex

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -21,22 +21,24 @@ const Home = () => {
   }
 
   return (
-    <div className='main'>
-      <h1 className="text-3xl mt-12 mb-8 font-bold">Bonjour et bienvenue !</h1>
-      <h2 className="text-2xl mb-14 font-bold">Tu pourras ici suivre le parcours des challenges Hetic</h2>
-      <h2 className="text-xl text-center mb-8">Tout est découpé étape par étape et tu auras les feedbacks instantanément !<br/>On a voulu que ce soit agréable et fluide, laisse-toi porter ;)</h2>
-      <button
-        onClick={handleClick}
-        type="submit"
-        className="w-96 text-center mt-6 py-3 rounded bg-white text-black hover:bg-green-400 focus:outline-none my-1"
-      >
-        C&#39;est parti !
-      </button>
-      <div className='lottieContainer'>
+    <div className='main flex gap-40'>
+      <div className='right'>
+        <h1 className="text-3xl mt-12 mb-8 font-bold">Bonjour et bienvenue !</h1>
+        <h2 className="text-2xl mb-10 font-bold">Tu pourras ici suivre le parcours des challenges Hetic</h2>
+        <h2 className="text-xl mb-8">Tout est découpé étape par étape et tu auras les feedbacks instantanément !<br/>On a voulu que ce soit agréable et fluide, laisse-toi porter ;)</h2>
+        <button
+          onClick={handleClick}
+          type="submit"
+          className="w-96 text-center mt-6 py-3 rounded bg-white text-black hover:bg-green-400 focus:outline-none my-1"
+        >
+          C&#39;est parti !
+        </button>
+      </div>
+      <div className='lottieContainer max-w-min'>
         <Lottie
           options={defaultOptions}
-          height={300}
-          width={300}
+          height={400}
+          width={350}
         />
       </div>
     </div>
